a424d59acb371e1f2cae46d7eb84edf62448ca45
JC Kim <jckim3@gmail.com>
2024-07-08 15:13:39 -0600

    add Get Rent type staus

--- HelloWorldApp/ContentView.swift
+++ HelloWorldApp/ContentView.swift
@@ -43,7 +43,18 @@ struct ContentView: View {
             }
 
             Button(action: {
-                // 세 번째 버튼이 눌렸을 때 메시지 변경
+                // 새로운 세 번째 버튼이 눌렸을 때 API 호출
+                fetchRoomRentStatus()
+            }) {
+                Text("Get Room Rent Status")
+                    .padding()
+                    .background(Color.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+                // 네 번째 버튼이 눌렸을 때 메시지 변경
                 message = "Pressed Button 2"
             }) {
                 Text("Button 2")
@@ -121,17 +132,52 @@ struct ContentView: View {
                        }
                    },
                    receiveValue: { salesResponse in
-                                      let formatter = NumberFormatter()
-                                      formatter.numberStyle = .decimal
-                                      formatter.maximumFractionDigits = 0 // 소수점 표시하지 않음
+                       let formatter = NumberFormatter()
+                       formatter.numberStyle = .decimal
+                       formatter.maximumFractionDigits = 0 // 소수점 표시하지 않음
 
-                                      let formattedTotalPrice = formatter.string(from: NSNumber(value: salesResponse.totalPrice)) ?? "\(salesResponse.totalPrice)"
-                                      let formattedTotalCreditPrice = formatter.string(from: NSNumber(value: salesResponse.totalCreditPrice)) ?? "\(salesResponse.totalCreditPrice)"
-                                      let totalSales = salesResponse.totalPrice + salesResponse.totalCreditPrice
-                                      let formattedTotalSales = formatter.string(from: NSNumber(value: totalSales)) ?? "\(totalSales)"
+                       let formattedTotalPrice = formatter.string(from: NSNumber(value: salesResponse.totalPrice)) ?? "\(salesResponse.totalPrice)"
+                       let formattedTotalCreditPrice = formatter.string(from: NSNumber(value: salesResponse.totalCreditPrice)) ?? "\(salesResponse.totalCreditPrice)"
+                       let totalSales = salesResponse.totalPrice + salesResponse.totalCreditPrice
+                       let formattedTotalSales = formatter.string(from: NSNumber(value: totalSales)) ?? "\(totalSales)"
 
-                                      message = "Current month sales - Cash: $\(formattedTotalPrice), Credit: $\(formattedTotalCreditPrice), Total: $\(formattedTotalSales)"
-                                  }
+                       message = "Current month sales - Cash: $\(formattedTotalPrice), Credit: $\(formattedTotalCreditPrice), Total: $\(formattedTotalSales)"
+                   }
+               )
+       }
+
+    func fetchRoomRentStatus() {
+        guard let url = URL(string: "http://192.168.0.192:81/api/motel/rooms/payment-types") else {
+            message = "Invalid URL"
+            return
+        }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+               .tryMap { result -> Data in
+                   guard let response = result.response as? HTTPURLResponse else {
+                       throw URLError(.badServerResponse)
+                   }
+                   print("Response status code: \(response.statusCode)")
+                   guard response.statusCode == 200 else {
+                       throw URLError(.badServerResponse)
+                   }
+                   return result.data
+               }
+               .decode(type: PaymentTypeCounts.self, decoder: JSONDecoder())
+               .receive(on: DispatchQueue.main)
+               .sink(
+                   receiveCompletion: { completion in
+                       switch completion {
+                       case .finished:
+                           break
+                       case .failure(let error):
+                           message = "Error: \(error.localizedDescription)"
+                           print("Error: \(error)")
+                       }
+                   },
+                   receiveValue: { paymentTypeCounts in
+                                          message = "Room Rent Status - Daily: \(paymentTypeCounts.da), Weekly: \(paymentTypeCounts.wk), Montly: \(paymentTypeCounts.mo), Week Voucher: \(paymentTypeCounts.wc), Master Lease: \(paymentTypeCounts.ml)"
+                                      }
                )
        }
 }
@@ -141,8 +187,17 @@ struct SalesResponse: Decodable {
     let totalCreditPrice: Double
 }
 
+struct PaymentTypeCounts: Decodable {
+    let da: Int
+    let wk: Int
+    let mo: Int
+    let wc: Int
+    let ml: Int
+}
+
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
