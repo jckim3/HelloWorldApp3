cbc2160a8c3aacb2f5cee04c8cbea215032e06d1
JC Kim <jckim3@gmail.com>
2024-07-08 11:09:30 -0600

    all

--- HelloWorldApp/ContentView.swift
+++ HelloWorldApp/ContentView.swift
@@ -1,1 +1,88 @@
+//
+//  ContentView.swift
+//  HelloWorldApp
+//
+//  Created by JC Kim on 7/1/24.
+//
 
+import SwiftUI
+import Combine
+
+struct ContentView: View {
+    @State private var message: String = "Hello, world! Carriage Motor Inn"
+    @State private var cancellable: AnyCancellable?
+
+    var body: some View {
+        VStack(spacing: 20) { // 간격을 추가하여 버튼 간의 간격을 벌림
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text(message)
+                .padding()
+
+            Button(action: {
+                // 첫 번째 버튼이 눌렸을 때 API 호출
+                fetchAvailableRoomsCount()
+            }) {
+                Text("Get # of Empty Room")
+                    .padding()
+                    .background(Color.blue)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+
+            Button(action: {
+                // 두 번째 버튼이 눌렸을 때 메시지 변경
+                message = "Pressed Button 2"
+            }) {
+                Text("Button 2")
+                    .padding()
+                    .background(Color.green)
+                    .foregroundColor(.white)
+                    .cornerRadius(10)
+            }
+        }
+        .padding()
+    }
+
+    func fetchAvailableRoomsCount() {
+        guard let url = URL(string: "http://192.168.0.192:81/api/motel/available-rooms-count") else {
+            message = "Invalid URL"
+            return
+        }
+        
+        cancellable = URLSession.shared.dataTaskPublisher(for: url)
+               .tryMap { result -> Data in
+                   guard let response = result.response as? HTTPURLResponse else {
+                       throw URLError(.badServerResponse)
+                   }
+                   print("Response status code: \(response.statusCode)")
+                   guard response.statusCode == 200 else {
+                       throw URLError(.badServerResponse)
+                   }
+                   return result.data
+               }
+               .decode(type: Int.self, decoder: JSONDecoder())
+               .receive(on: DispatchQueue.main)
+               .sink(
+                   receiveCompletion: { completion in
+                       switch completion {
+                       case .finished:
+                           break
+                       case .failure(let error):
+                           message = "Error: \(error.localizedDescription)"
+                           print("Error: \(error)")
+                       }
+                   },
+                   receiveValue: { availableRoomsCount in
+                       message = "Available rooms count: \(availableRoomsCount)"
+                   }
+               )
+       }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
